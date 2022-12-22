e6b40415a84181057e3ea04f588d73a1c5d6c425
Yakir Gagnon <12.yakir@gmail.com>
2022-12-22 11:20:52 +0100

    cleaned

--- client.jl
+++ client.jl
@@ -1,30 +1,63 @@
-using HTTP, JSON3, ProgressMeter 
 using Test
+using ProgressMeter, HTTP
+using HTTP: send, post, receive
 
-function http_request()
-    resp = HTTP.get("http://127.0.0.1:8081/http")
-    @test 0 ≤ JSON3.read(resp.body) ≤ 1
-end
-
-function websocket_request()
-    HTTP.WebSockets.open("http://127.0.0.1:8081/websocket") do ws
-        pkg = rand(UInt8, 10)
-        HTTP.send(ws, pkg)
-        msg = HTTP.receive(ws)
-        @test reverse(pkg) == msg
-    end
-end
-
-function requests()
-    http_request()
-    websocket_request()
-end
+n = 1000
 
 @info "running client with $(Threads.nthreads()) threads"
-n = 1000
+
+##################### HTTP #######################
+
+const header = ["Content-Type" => "application/octet-stream"]
+
+function http_request()
+    bytes = rand(UInt8, 5)
+    resp = post("http://127.0.0.1:8081/http", header; body = bytes)
+    @test resp.status == 200 && resp.body == reverse(bytes)
+end
+
+@info "testing multi-threaded http endpoint"
 p = Progress(n)
 Threads.@threads for i in 1:n
-    requests()
+    http_request()
     next!(p)
 end
 
+#################### WebSocket ###################
+
+function websocket_function(ws)
+    # @info "I'm running on thread #$(Threads.threadid())"
+    pkg = rand(UInt8, 10)
+    send(ws, pkg)
+    msg = receive(ws)
+    @test reverse(pkg) == msg
+    return 
+end
+
+@info "testing single-threaded websocket endpoint"
+HTTP.WebSockets.open("http://127.0.0.1:8081/websocket") do ws
+    @showprogress for i in 1:n
+        websocket_function(ws)
+    end
+end
+
+@info "testing multi-threaded single websocket endpoint"
+HTTP.WebSockets.open("http://127.0.0.1:8081/websocket") do ws
+    p = Progress(n)
+    Threads.@threads for i in 1:n
+        websocket_function(ws)
+        next!(p)
+    end
+end
+
+@info "testing single-threaded multiple websocket endpoints"
+@showprogress for i in 1:n
+    HTTP.WebSockets.open(websocket_function, "http://127.0.0.1:8081/websocket")
+end
+
+@info "testing multi-threaded multiple websocket endpoints"
+p = Progress(n)
+Threads.@threads for i in 1:n
+    HTTP.WebSockets.open(websocket_function, "http://127.0.0.1:8081/websocket")
+    next!(p)
+end
--- server.jl
+++ server.jl
@@ -1,36 +1,50 @@
-using HTTP, JSON3, ThreadPools
+using HTTP
+using HTTP: Response, register!
 
-header = ["Content-Type" => "text/json"]
-body = JSON3.write(rand())
+const ROUTER = HTTP.Router()
 
-http_handler(request) = HTTP.Response(200, header; body)
+server = HTTP.serve!(ROUTER; stream=true)
+
+@info "running server with $(Threads.nthreads()) threads"
+
+##################### HTTP #######################
+
+const header = ["Content-Type" => "application/octet-stream"]
+
+function async_middleware(handler)
+    function (req)
+        tsk = Threads.@spawn handler(req)
+        return fetch(tsk)
+    end
+end
+
+function expensive_function(bytes)
+    @info "I'm running on thread #$(Threads.threadid())"
+    return reverse(bytes)
+end
+
+function handler(request)
+    bytes = request.body
+    body = expensive_function(bytes)
+    Response(200, header; body)
+end
+
+register!(ROUTER, "/http", handler |> async_middleware |> HTTP.streamhandler)
+
+#################### WebSocket ###################
 
 function websocket_handler(stream)
     request = stream.message
     return WebSockets.upgrade(ws -> websocket_handler(request, ws), stream)
 end
 
-function fun(bytes)
-    @info "I'm running on thread #$(Threads.threadid())"
-    return reverse(bytes)
-end
-
 function websocket_handler(request, websocket)
     for bytes in websocket
-        # tsk = Threads.@spawn fun(bytes)
-        tsk = spawnbg() do
-            fun(bytes)
-        end
+        tsk = Threads.@spawn expensive_function(bytes)
         WebSockets.send(websocket, fetch(tsk))
     end
 end
 
-const ROUTER = HTTP.Router()
-HTTP.register!(ROUTER, "/http", HTTP.streamhandler(http_handler))
-HTTP.register!(ROUTER, "/websocket", websocket_handler)
-
-server = HTTP.serve!(ROUTER; stream=true)
-
-@info "running server with $(Threads.nthreads()) threads"
+register!(ROUTER, "/websocket", websocket_handler)
 
 wait(server)
